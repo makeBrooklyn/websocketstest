b01796a8cbca55cc43213d99a1532c472c43a923
Jim Barry <makeBrooklyn@users.noreply.github.com>
2025-06-27 16:06:55 -0400

    Looks liek the mobile app tried to make it's own seperate repo ... adding it here

---
+++ mobile/App.tsx
@@ -0,0 +1,43 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import { NewAppScreen } from '@react-native/new-app-screen';
+import {
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+function App() {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <Text style={styles.headerText}>THIS IS NEW</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'black',
+    color: 'white',
+  },
+  headerText: {
+    fontSize: 20,
+    fontWeight: '600',
+    color: 'white',
+  },
+});
+
+export default App;
