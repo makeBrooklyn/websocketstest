e2091aeb8e9c2bcd0876e1a2975cc11859bdb922
Jim Barry <makeBrooklyn@users.noreply.github.com>
2025-06-27 16:02:53 -0400

    Got this accepting connnectionnns as a websocket server

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,15 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+io.on('connection', (socket) => {
+    console.log('A user connected');
 });
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+server.listen(port, () => {
+   console.log(`Server is running on port: ${port}`);
 });
+
